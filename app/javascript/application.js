b0de7bb123ecc29b05b73705e18548f3fbd4ed71
David Yarbro <yarbro@codeforaction.org>
2025-03-29 19:37:32 -0500

    Adds the index page with logo and github link

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "@picocss/pico/css/pico.css";
+import "../assets/stylesheets/application.css";
 import "./controllers"
